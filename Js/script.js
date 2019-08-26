227d40d85eb052d6782ebcd18442658169f9f671
cekipele <cephalo67@gmail.com>
2019-08-26 15:38:53 +0300

    add initial changes

--- Js/script.js
+++ Js/script.js
@@ -17,4 +17,21 @@ $(document).ready(function(){
      $(".img3").toggle();
      $(".par3").toggle();
    })
+
+   //Hower
+   $("#col-1").hover(function() {
+     $("#safe").fadeTo("slow",0.3);
+     $("#text1").fadeTo("slow",1);
+   },function() {
+     $("#safe").fadeTo("slow",1);
+     $("#text1").fadeTo("slow",0);
+   });
+
+   $("#col-2").hover(function() {
+     $("#java").fadeTo("slow",0.3);
+     $("#text2").fadeTo("slow",1);
+   },function() {
+     $("#java").fadeTo("slow",1);
+     $("#text2").fadeTo("slow",0);
+   });
 });
