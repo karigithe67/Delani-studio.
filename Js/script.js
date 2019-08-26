c58df07b768fa74e9796195425d932fa86ba2298
cekipele <cephalo67@gmail.com>
2019-08-26 13:32:24 +0300

    add initial changes

---
+++ Js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $(".text").click(function(){
+
+    $(".img1").toggle();
+    $(".par1").toggle();
+  });
+
+  $(".type").click(function(){
+
+    $(".img2").toggle();
+    $(".par2").toggle();
+  });
+
+   $(".value").click(function(){
+
+     $(".img3").toggle();
+     $(".par3").toggle();
+   })
+});
